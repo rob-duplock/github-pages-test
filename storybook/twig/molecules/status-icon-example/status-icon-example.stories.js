863e836b8a0ab1d2a1ce020bb519da861608bb8e
Avitus Shim <ashim@dhmedia.com.au>
2020-03-13 16:21:02 +0800

    Create molecule status icon for testing

---
+++ storybook/twig/molecules/status-icon-example/status-icon-example.stories.js
@@ -0,0 +1,10 @@
+import StatusIconExample from './status-icon-example.html.twig';
+
+StatusIconExample.data = {
+  parameters: {
+    icon: ['info'],
+    types: ['success'],
+  }
+};
+
+export default StatusIconExample;
