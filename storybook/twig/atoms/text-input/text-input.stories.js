824d2f7a73417d352c20231a9a622a7bf2191eb9
Avitus Shim <ashim@dhmedia.com.au>
2020-03-19 13:16:11 +0800

    Update text input to show error text

--- storybook/twig/atoms/text-input/text-input.stories.js
+++ storybook/twig/atoms/text-input/text-input.stories.js
@@ -1,15 +1,23 @@
 import { storiesOf } from '@storybook/html';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import drupalAttribute from 'drupal-attribute';
 import TextInput from './text-input.html.twig';
 
 storiesOf('Atoms', module)
+  .addDecorator(withKnobs)
   .add('TextInput', () => {
     let attributes = {
       id: 'default-id',
       name: 'default-name',
       class: ['text-input'],
-      placeholder: 'Enter value',
+      placeholder: text('Placeholder', 'Enter value'),
     };
+
+    let errorClass = boolean('Error', false);
+    if (errorClass) {
+      attributes.class.push('error');
+      attributes.placeholder = text('Error message', 'Error');
+    }
 
     let variables = {
       attributes: new drupalAttribute(Object.entries(attributes)),
