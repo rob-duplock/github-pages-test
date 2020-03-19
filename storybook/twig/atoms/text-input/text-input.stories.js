e9088098d122a4c4472c71b69d67ad641894a868
Avitus Shim <ashim@dhmedia.com.au>
2020-03-19 17:01:08 +0800

    Remove drupal attribute

--- storybook/twig/atoms/text-input/text-input.stories.js
+++ storybook/twig/atoms/text-input/text-input.stories.js
@@ -1,6 +1,5 @@
 import { storiesOf } from '@storybook/html';
 import { withKnobs, text, boolean } from '@storybook/addon-knobs';
-import drupalAttribute from 'drupal-attribute';
 import TextInput from './text-input.html.twig';
 
 storiesOf('Atoms', module)
@@ -9,19 +8,15 @@ storiesOf('Atoms', module)
     let attributes = {
       id: 'default-id',
       name: 'default-name',
-      class: ['text-input'],
+      classes: ['text-input'],
       placeholder: text('Placeholder', 'Enter value'),
     };
 
     let errorClass = boolean('Error', false);
     if (errorClass) {
-      attributes.class.push('error');
+      attributes.classes.push('error');
       attributes.placeholder = text('Error message', 'Error');
     }
 
-    let variables = {
-      attributes: new drupalAttribute(Object.entries(attributes)),
-    };
-
-    return TextInput(variables);
+    return TextInput(attributes);
   });
